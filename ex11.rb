bfd7e4215d208d369420319abfb951e633327fde
Samuel Tillman <SamuelTillmanSRE@gmail.com>
2020-03-23 10:37:43 -0400

    Adding ex10-11

---
+++ ex11.rb
@@ -0,0 +1,8 @@
+print "What's your name? "
+name = gets.chomp
+print "What's your occupation? "
+job = gets.chomp
+print "How long? "
+time = gets.chomp
+
+puts "So, you're name is #{name}, and you've been a #{job} for #{time}."
